e1529a9f9b91ab8d5e79e55550c1b0c8c717aa71
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2019-07-24 22:55:20 +0200

    🔧

--- src/app/shared/store/store.module.ts
+++ src/app/shared/store/store.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule as NgrxStoreModule } from '@ngrx/store';
-import { metaReducers, reducers } from './reducers';
+import { reducers } from './reducers';
 
 @NgModule({
   imports: [
     NgrxStoreModule.forRoot(reducers, {
-      metaReducers,
       runtimeChecks: {
         strictStateImmutability: true,
         strictActionImmutability: true
