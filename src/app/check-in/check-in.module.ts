b5ac89251d321b97fe9c24e650f6481b42959463
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2019-07-24 17:37:28 +0200

    feat: : 🤔

--- src/app/check-in/check-in.module.ts
+++ src/app/check-in/check-in.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { CheckInComponent } from './check-in.component';
 
 @NgModule({
   declarations: [CheckInComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   exports: [CheckInComponent]
 })
 export class CheckInModule {}
