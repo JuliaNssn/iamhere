5dba61e34a45bda4f81bd7cfcb3d7e72ef5ec2ad
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2019-07-24 17:45:07 +0200

    feat: add user component

--- src/app/check-in/check-in.module.ts
+++ src/app/check-in/check-in.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { UserModule } from '../shared/user/user.module';
 import { CheckInComponent } from './check-in.component';
 
 @NgModule({
   declarations: [CheckInComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, UserModule],
   exports: [CheckInComponent]
 })
 export class CheckInModule {}
