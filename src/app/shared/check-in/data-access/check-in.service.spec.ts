76b39d886d08e63de208c0f399ce56ea22196b6c
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2019-07-25 10:57:21 +0200

    🛠

--- src/app/shared/check-in/data-access/check-in.service.spec.ts
+++ src/app/shared/check-in/data-access/check-in.service.spec.ts
@@ -1,12 +1,12 @@
-import { HttpClient } from "@angular/common/http";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { TestBed } from "@angular/core/testing";
-import * as faker from "faker";
-import { CHECK_IN_SOURCE_API_URL } from "./check-in.model";
-import { CheckInService } from "./check-in.service";
-import { userAndRoomFactory } from "./testing/checkin.data";
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import * as faker from 'faker';
+import { CHECK_IN_SOURCE_API_URL } from './check-in.model';
+import { CheckInService } from './check-in.service';
+import { userAndRoomFactory } from './testing/checkin.data';
 
-describe("CheckinService", () => {
+describe('CheckinService', () => {
   const apiUrl: string = faker.internet.url();
   let service: CheckInService;
   let httpClient: HttpClient;
@@ -29,12 +29,12 @@ describe("CheckinService", () => {
     httpClient = TestBed.get(HttpClient);
   });
 
-  it("should be created", () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  describe("checkIn", () => {
-    it("shoould call the httpClient post method", () => {
+  describe('checkIn', () => {
+    it('shoould call the httpClient post method', () => {
       const userAndRoom = userAndRoomFactory.build();
       httpClient.post = jest.fn();
 
@@ -47,8 +47,8 @@ describe("CheckinService", () => {
     });
   });
 
-  describe("checkOut", () => {
-    it("shoould call the httpClient post method", () => {
+  describe('checkOut', () => {
+    it('shoould call the httpClient post method', () => {
       const userAndRoom = userAndRoomFactory.build();
       httpClient.post = jest.fn();
 
